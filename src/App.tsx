401a9315b24c736db9a2c29a04383cc873377880
Dimas Dwi Putra <dimsdputra@gmail.com>
2025-07-10 08:16:40 +0700

    testing rendering

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { Toaster } from "./components/ui/Sonner";
 import AuthInit from "./pages/auth/AuthInit";
 import AppRoutes from "./pages/Routes/AppRoutes";
 import { useThemeStore } from "./store/themeStore";
-import 'tippy.js/dist/tippy.css'; 
+import "tippy.js/dist/tippy.css";
 
 const { BASE_URL } = import.meta.env;
 const queryClient = new QueryClient();
@@ -16,7 +16,11 @@ const App = () => {
   const { theme } = useThemeStore();
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <QueryClientProvider client={queryClient}>
+      <div>
+        <h1>Hello Vercel!</h1>
+        <p>If you see this, my app is rendering!</p>
+      </div>
+      {/* <QueryClientProvider client={queryClient}>
         <BrowserRouter basename={BASE_URL}>
           <div className={theme}>
             <section className="bg-base-200 h-screen w-screen overflow-x-hidden overflow-y-auto custom-scrollbar">
@@ -29,7 +33,7 @@ const App = () => {
             </section>
           </div>
         </BrowserRouter>
-      </QueryClientProvider>
+      </QueryClientProvider> */}
     </Suspense>
   );
 };
